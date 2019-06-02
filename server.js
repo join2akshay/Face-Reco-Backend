28bcd717390531cdc4031f9eaeae0e00194eafe1
tvibhu12 <tvibhu12@gmail.com>
2019-06-02 16:51:05 +0530

    setup registration connection to the database

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ const db=knex({
       database : 'smart-brains'
     }
   });
- db.select('*').table('users').then(data=>{console.log(data)});
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -22,43 +21,41 @@ res.send(database.users);
 })   
 
 app.post('/Signin',(req,res)=>{
-    // console.log(req.body);
-    // bcrypt.compare("cookies", '$2a$10$ZL9RdsjffcNm3BjuXtt1Se1LNeefIFyd2NQmryX3a4KXlG3VTBP1i', function(err, res) {
-    //      console.log('first guess',res);//res == true
-    //      });
-    // bcrypt.compare("veggies",'$2a$10$ZL9RdsjffcNm3BjuXtt1Se1LNeefIFyd2NQmryX3a4KXlG3VTBP1i', function(err, res) {
-    //     console.log('second guess',res);//res = false
-    //      });
-    if(req.body.email===database.users[0].email &&req.body.password===database.users[0].password)
-    {
-       
-        res.json(database.users[0]);
-      
-
-    }else{
-        res.status(400).json('Error');
-      
-    }
-    
+ 
 })
 
 app.post('/Register',(req,res)=>{
     const {email,name,password}=req.body;
-    bcrypt.hash(password, null, null, function(err, hash) {
-       console.log(hash);//Store hash in your password DB.
-        });
-    db('users').returning('*')
-    .insert(
-        {
-            email:email,
-            name:name,
-            joined:new Date()
-        }
-    ).then(user=>{
-        res.json(user[0]);
-    }).catch(err=>res.status(400).json('unable to connent'))
+    const hash=bcrypt.hashSync(password);
+           //Store hash in password DB.
+    db.transaction(trx=>{
+trx.insert({
+        hash:hash,
+        email:email
+})
+    .into('login')
+    .returning('email')
+    .then(loginEmail=>{
+        
+       return trx('users')
+        .returning('*')
+        .insert(
+            {
+                email:loginEmail[0],
+                name:name,
+                joined:new Date()
+            }
+        ).then(user=>{
+            res.json(user[0]);
+        })
+    }).then(trx.commit)
+      .catch(trx.rollback)
+    })
+.catch(err=>res.status(400).json('unable to connent'))
    // res.json(database.users[database.users.length-1]);
 })
+
+
 app.put('/profile/:id',(req,res)=>{
     const {id}=req.params;
     db.select('*').from('users').where({id}).then(user=>{
@@ -71,22 +68,6 @@ app.put('/profile/:id',(req,res)=>{
     })
 })
 
-app.put('/image',(req,res)=>{
-    const{id}=req.body; 
-    database.users.forEach(user => {
-        if(user.id===id)
-        {
-            found=true;
-            user.entries++;
-           return res.json(user.entries);
-        }
-        
-    })
-    if(!found)
-    {
-        res.status(400).json('no such data');
-    };
-})
 app.listen(3001,()=>{
    
 });
