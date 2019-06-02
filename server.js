64e9af5f7af012b045c946995ead86689da8333e
tvibhu12 <tvibhu12@gmail.com>
2019-06-02 16:52:11 +0530

    setup signin  connection to the database

--- server.js
+++ server.js
@@ -17,11 +17,27 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/',(req,res)=>{
-res.send(database.users);
+
 })   
 
 app.post('/Signin',(req,res)=>{
  
+    db.select('email','hash').from('login')
+    .where('email','=',req.body.email)
+    .then(data=>{
+        const isValid = bcrypt.compareSync(req.body.password,data[0].hash);
+        if(isValid)
+        {
+            return db.select('*').from('users')
+            .where('email','=',req.body.email)
+            .then(user=>{
+                res.json(user[0])
+            })
+            .catch(err=>res.status(400).json('unable to get user'))
+        }
+    })
+    .catch(err=>res.status(400).json('wrong Details entered'));
+
 })
 
 app.post('/Register',(req,res)=>{
